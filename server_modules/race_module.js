40f24245b197e83bb1fcdad1a3f4d672bae26818
H3rN4n <desouza030689@gmail.com>
2014-07-13 04:24:02 -0300

    socket actions exported

--- server_modules/race_module.js
+++ server_modules/race_module.js
@@ -1,3 +1,42 @@
+var http = require('../server.js');
+var moniker = require('moniker');
+var io = require('socket.io')(http);
+
+// socket actions ======================================================================
+
+var users = [];
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    var random_name = moniker.choose();
+    socket.emit('login_as', {'username' :random_name, 'id': socket.id});
+    race_module.id = socket.id;
+    race_module.socket = socket;
+    users[socket.id] = race_module;
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('start_race', function(data){
+        var user_race = users[data.id];
+        user_race.start_race();
+    });
+
+    socket.on('stop_race', function(data){
+        var user_race = users[data.id];
+        user_race.stop_race();
+    });
+
+    socket.on('client_horse_movement', function(data){
+        console.log(data);
+        //console.log(username + 'is moving');
+    });
+  
+});
+
+
 /**
  * Provides the base Widget class...
  *
